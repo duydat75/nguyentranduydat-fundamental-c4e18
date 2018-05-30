34149c773ead79149bd0cde85e9a433b1704b34c
Nguyễn Đạt <datgreen@Dats-MacBook-Pro.local>
2018-05-30 13:58:36 +0700

    Initial commit

---
+++ Session03/homework/break.py
@@ -0,0 +1,11 @@
+n = 0
+guess= int(input("Input random number: "))
+while True:
+    if n == guess:
+        print ("Dat dep trai")
+        break
+    else :
+        print ("Dat dep trai hon")
+        break
+mylist = [69,"Dat dep trai",96]
+print (mylist)
