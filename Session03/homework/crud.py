5c06e074e92d0b444afc9ffcb9372ba327dad5ad
Nguyễn Đạt <datgreen@Dats-MacBook-Pro.local>
2018-05-30 18:04:13 +0700

    Initial commit

--- Session03/homework/crud.py
+++ Session03/homework/crud.py
@@ -3,18 +3,18 @@ print(*list(word),sep=', ')
 items =['T-Shirt', 'Sweater']
 for i in range (4):
     choice = input("What do you want: ")
-    if choice == "R":
+    if choice in ["r","R"]:
         print ("Out items: ",*items, sep=', ')
-    if choice == "C":
+    if choice in ["C","c"]:
         new = input("Enter new item: ")
         items.append(new)
         print ("Out items: ",*items,sep=', ')    
-    elif choice == "U":
+    elif choice in ["U","u"]:
         position = int(input ("Update position: "))
         update_item = input("New item ? ")
         items[position-1]=update_item
         print (*items, sep=', ')
-    elif choice == "D":
+    elif choice in ["D","d"]:
         delete = int(input("Delete position ? "))
         del items[delete-1]
         print (*items, sep=', ')
--- Session03/homework/sheeps.py
+++ Session03/homework/sheeps.py
@@ -1,32 +1,26 @@
 flock = [5,7,300,90,24,50,75]
 print ("Hello my name is Hiep and here is my flock: \n",flock)
-size_max=flock[0]
+size_max = 0
 a=0
 total = 0
-for i in range (len(flock) - 1):
-    if flock[i+1] > size_max:
-        size_max= flock [i+1]
-        a=i+1
-flock[a]= 8
+size_max = max(flock)
+update = flock.index(size_max)
+flock[update]=8
 print ("Now my biggest sheep size", size_max,"let's shear it")
 print (flock)
-
 for i in range (3):
     for j in range (len(flock)):
         flock[j]+=50
         if i == 2:
-            total += flock [j]
-    size_max=flock[0]
-    a=0
-    for q in range (len(flock) - 1):
-        if flock[q+1] > size_max:
-            size_max= flock [q+1]
-            a=q+1
-    print ("MONTH", i+1)
-    print ("1 month has passed, now here is my flock\n", flock)
-    flock[a]= 8     
-    print ("Now my biggest sheep size", size_max,"let's shear it","\nAfter shearing here is my flock: ")
-    print (flock)   
-print ("My flock has size in total: ",total,"\nI would get",total, "* 2$ =", total *2,"$")
-
-
+            total += flock[j]
+    print ("\nMonth",i+1,":")
+    print ("One month has passed, now here is my flock: \n",flock)
+    while i < 2:
+        size_max = max(flock)
+        update = flock.index(size_max)
+        flock[update]=8
+        print ("Now my biggest sheep size", size_max,"let's shear it","\nAfter shearing here is my flock: ")
+        print (flock)
+        break
+print ("My flock has size inc total:",total)
+print ("I would get", total,"* 2$ =",total*2, "$")
