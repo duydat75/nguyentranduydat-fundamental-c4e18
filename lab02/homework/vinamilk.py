b5c50040f1a3ffac92ef0e52c49565de8d4b8b22
Nguyễn Đạt <datgreen@Dats-MacBook-Pro.local>
2018-06-14 15:26:20 +0700

    Dat hihihi

--- lab02/homework/vinamilk.py
+++ lab02/homework/vinamilk.py
@@ -1,23 +1,24 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from htmldom import htmldom
 import pyexcel
 
-url = 'http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2017/3/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn'
-html_content = urlopen('http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2017/3/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn').read().decode('utf-8')
-
-soup = BeautifulSoup(html_content,'html.parser')
-table = soup.find('table', id='tableContent')
-
-td_list = table.find_all("td","b_r_c")
-
+dom=htmldom.HtmlDom("http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2018/1/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn")
+dom = dom.createDom()
+p = dom.find("td[class=b_r_c]")
 record = []
-for i in range (0,len(td_list)-4,6):
+dat = []
+for i in range (0,p.length(),1):
+    if p[i].children()[0] == None:
+        record.append(p[i].text().replace("&nbsp",'').replace("\r",'').replace("\n",''))
+for i in range (0,len(record),5):
     data={}
-    data['Content'] = td_list[i].string
-    data['Quý 2-2017'] = td_list[i+3].string
-    data['Quý 3-2017'] = td_list[i+4].string
-    data['Quý 4-2017'] = td_list[i+1].string
-    data['Quý 1-2018'] = td_list[i+2].string
-    record.append(data)
-    
-pyexcel.save_as(records=record, dest_file_name="vinamils.xlsx")
+    data['Content'] = record[i] 
+    data['Quý 2-2017'] = record[i+1] 
+    data['Quý 3-2017'] = record[i+2] 
+    data['Quý 4-2017'] = record[i+3] 
+    data['Quý 1-2018'] = record[i+4] 
+    dat.append(data)
+print (dat)
+pyexcel.save_as(records=dat, dest_file_name="vinamils.xlsx")
+
+
+
--- test/test.py
+++ test/test.py
@@ -1,17 +1,24 @@
-map_sokoban = {
-    "size_x":10,
-    "size_y":10
-}
-destinations = [
-    {"x":5,"y":5},
-    {"x":4,"y":3},
-    {"x":8,"y":8}
-    ]
-for y in range (map_sokoban["size_y"]):
-    for x in range (map_sokoban["size_x"]):
-        pic = '- '
-        for des in destinations:
-            if des["x"] == x and des["y"] == y:
-                pic = 'D '
-        print (pic, end='')
-    print()
+from htmldom import htmldom
+import pyexcel
+
+dom=htmldom.HtmlDom("http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2018/1/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn")
+dom = dom.createDom()
+p = dom.find("td[class=b_r_c]")
+record = []
+dat = []
+for i in range (0,p.length(),1):
+    if p[i].children()[0] == None:
+        record.append(p[i].text().replace("&nbsp",'').replace("\r",'').replace("\n",''))
+for i in range (0,len(record),5):
+    data={}
+    data['Content'] = record[i] 
+    data['Quý 2-2017'] = record[i+1] 
+    data['Quý 3-2017'] = record[i+2] 
+    data['Quý 4-2017'] = record[i+3] 
+    data['Quý 1-2018'] = record[i+4] 
+    dat.append(data)
+print (dat)
+pyexcel.save_as(records=dat, dest_file_name="vinamils.xlsx")
+
+
+
