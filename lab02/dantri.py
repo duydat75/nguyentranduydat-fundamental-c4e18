b39da4c8d4df6e21bbc3032fcf0c836799c8d60a
datgreen <duydat75@gmail.com>
2018-07-29 05:00:29 +0700

    .

--- lab02/dantri.py
+++ lab02/dantri.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 #1.1 Create a connection
 
 # html = urlopen("http://dantri.com.vn/")
-url = 'http://dantri.com.vn/'
+
 # #1.2 Read 
 
 # data = html.read()
@@ -13,18 +13,22 @@ url = 'http://dantri.com.vn/'
 # #1.3 Decode 
 
 # html_content = data.decode('utf-8')
-html_content = urlopen("http://dantri.com.vn/").read().decode('utf-8')
-print (html_content)
+# html_content = urlopen('anchoi.html').read().decode('utf-8')
 
-# f = open("dantri.html",'w')
+
+html_content = open("anchoi.html",'rb')
 # f.write(html_content)
 # f.close()
-
+# print(html_content)
 #2 extract ROI (Region of interest)
 
-# soup = BeautifulSoup(html_content, 'html.parser')
-# # print (soup.prettify())
+soup = BeautifulSoup(html_content, 'html.parser')
+# print(soup)
 
+li = soup.find("li", "ldc-item ng-scope")
+
+
+print (li)
 # ul = soup.find("ul", "ul1 ulnew")
 # # print (ul.prettify())
 # record = []
